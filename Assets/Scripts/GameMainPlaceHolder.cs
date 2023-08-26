ca9c11cc32ada2db4b5a43201ff6172b3dc98f8b
LovelyCatHyt <hyt@paracraft.club>
2023-08-27 00:57:21 +0800

    init: Basic Project Structure

---
+++ Assets/Scripts/GameMainPlaceHolder.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexFlow.GameMain
+{
+#if UNITY_EDITOR
+
+    /// <summary>
+    /// just an empty class to trigger csproj file generating.
+    /// </summary>
+    internal static class GameMainPlaceHolder
+    {
+
+    }
+#endif
+}
---
+++ Assets/Scripts/NativeCore/NativeCorePlaceHolder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexFlow.NativeCore
+{
+#if UNITY_EDITOR
+    /// <summary>
+    /// Just an empty class to trigger csproj file generating.
+    /// </summary>
+    internal static class NativeCorePlaceHolder
+    {
+
+    }
+#endif
+}
