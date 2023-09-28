35aafe4a672c08ce73965f9119f57100713398e7
Hyt <69683382@qq.com>
2023-09-28 17:36:53 +0800

    refactor: 简化函数接口名
    反正类型不同可以区分, 名字相同方便无缝切换

--- Assets/Scripts/NativeCore/MeshModifier.cs
+++ Assets/Scripts/NativeCore/MeshModifier.cs
@@ -13,7 +13,7 @@ namespace HexFlow.NativeCore
         internal static extern unsafe void SetColorGridMap(Color* colors, Color* gridColors, int vertPerCell, int length);
 
         [DllImport(DllName, EntryPoint = "set_vert_color32_gridmap")]
-        internal static extern unsafe void SetColor32GridMap(Color32* colors, Color32* gridColors, int vertPerCell, int length);
+        internal static extern unsafe void SetColorGridMap(Color32* colors, Color32* gridColors, int vertPerCell, int length);
 
         /// <summary>
         /// 以单元格为最小单位设置网格颜色, 精度为<see cref="Color"/>, 即4个float
@@ -51,7 +51,7 @@ namespace HexFlow.NativeCore
         /// <summary>
         /// 以单元格为最小单位设置网格颜色, 精度为<see cref="Color32"/>, 即4个uint8
         /// </summary>
-        public static void SetMeshColor32(Mesh mesh, HexMeshType type, NativeArray<Color32> array)
+        public static void SetMeshColor(Mesh mesh, HexMeshType type, NativeArray<Color32> array)
         {
             if (!mesh)
             {
@@ -66,11 +66,11 @@ namespace HexFlow.NativeCore
                 {
                     case HexMeshType.UniformTriangle:
                         colors = new NativeArray<Color32>(18 * array.Length, Allocator.Temp);
-                        SetColor32GridMap(colors.Get(), array.Get(), 18, array.Length);
+                        SetColorGridMap(colors.Get(), array.Get(), 18, array.Length);
                         break;
                     case HexMeshType.SymmetricalLeastTriangle:
                         colors = new NativeArray<Color32>(12 * array.Length, Allocator.Temp);
-                        SetColor32GridMap(colors.Get(), array.Get(), 12, array.Length);
+                        SetColorGridMap(colors.Get(), array.Get(), 12, array.Length);
                         break;
                     default:
                         throw new ArgumentException($"Invalid HexMeshType: {type}");
