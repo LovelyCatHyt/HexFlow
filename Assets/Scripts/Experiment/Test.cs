e00e3ebabdbd16874a3d9ba0d61ccc6b191c5457
LovelyCatHyt <69683382@qq.com>
2023-10-03 23:23:09 +0800

    dep: 更新 Unitilities 至 13dbd0, Experiment 模块补充对 Unitilities 的依赖

--- Assets/Scripts/Experiment/Test.cs
+++ Assets/Scripts/Experiment/Test.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
+using Unitilities.Serialization;
+using System.IO;
 
-[ExecuteAlways]
 public class Test : MonoBehaviour
 {
-    public Vector3 Size = Vector3.one;
-
-    private BoxCollider _collider;
+    public struct MyStruct
+    {
+        public string Name;
+        public string Value;
+    }
 
     private void Awake()
     {
-        _collider = GetComponent<BoxCollider>();
+
+    }
+
+    [ContextMenu("Quick Test")]
+    public void QuickTest()
+    {
+        MyStruct obj = new MyStruct { Name = "name", Value = "value" };
+        const string fileName = "Test object";
+        PersistentDataManager.SaveObjectAsText(obj, fileName, DataScope.Save);
+        var path = PersistentDataManager.GetFullFilePath(fileName, DataScope.Save);
+
+        obj = PersistentDataManager.LoadObjectFromText<MyStruct>(fileName, DataScope.Save);
+        Debug.Log($"Directory.Exist: {Directory.Exists(path)} Name: {obj.Name}, Value: {obj.Value}");
     }
 }
